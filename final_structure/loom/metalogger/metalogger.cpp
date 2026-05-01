1ba0c4b0b25d77bebab2c1da7c5ec1f4a0bc0de4
Lmp-off <piv.lip@mail.ru>
2026-05-01 12:49:49 +0000

    add color highlighting to metalogger output

--- final_structure/loom/metalogger/metalogger.cpp
+++ final_structure/loom/metalogger/metalogger.cpp
@@ -2,37 +2,51 @@
 #include <iostream>
 #include <ctime>
 
-namespace metalog{
+namespace metalog {
 
+static bool g_logger_active = true;
 
-bool g_logger_active = true;
-LogLevel g_logger_level = LOG_INFO;
-
-void logger_init(bool active, LogLevel level) {
+void set_logger_active(bool active) {
     g_logger_active = active;
-    g_logger_level = level;
 }
 
-void toggle_logger(bool activate) {
-    g_logger_active = activate;
+bool is_logger_active() {
+    return g_logger_active;
 }
 
-void set_logger_level(LogLevel level) {
-    g_logger_level = level;
+const char* get_level_name(log_level lv) {
+    switch (lv) {
+        case LOG_ERROR:   return "[ERROR]";
+        case LOG_WARNING: return "[WARN] ";
+        case LOG_INFO:    return "[INFO] ";
+        case LOG_DEBUG:   return "[DEBUG]";
+        case LOG_TRACE:   return "[TRACE]";
+        default:          return "[????]";
+    }
 }
 
-void write_log(const std::string& s, LogLevel lv) {
+const char* get_color(log_level lv) {
+    switch (lv) {
+        case LOG_ERROR:   return "\033[31m";
+        case LOG_WARNING: return "\033[33m";
+        case LOG_INFO:    return "\033[32m";
+        case LOG_DEBUG:   return "\033[36m";
+        case LOG_TRACE:   return "\033[90m";
+        default:          return "\033[0m";
+    }
+}
+
+void write_log(const std::string& s, log_level lv) {
     if (!g_logger_active) return;
-    if (lv > g_logger_level) return;
-    
-    const char* level_names[] = {"[ERROR]", "[WARN] ", "[INFO] ", "[DEBUG]", "[TRACE]"};
     
     time_t now = time(nullptr);
     struct tm* tm_info = localtime(&now);
     char time_buf[20];
     strftime(time_buf, sizeof(time_buf), "%H:%M:%S", tm_info);
     
-    std::cout << "[" << time_buf << "] " << level_names[lv] << " " << s << std::endl;
+    std::cout << "[" << time_buf << "] "
+              << get_color(lv) << get_level_name(lv) << "\033[0m "
+              << s << std::endl;
 }
 
 }
--- final_structure/loom/metalogger/metalogger.h
+++ final_structure/loom/metalogger/metalogger.h
@@ -3,9 +3,15 @@
 
 #include <string>
 
+#define METALOG_ERROR(msg) metalog::write_log(msg, metalog::LOG_ERROR)
+#define METALOG_WARNING(msg) metalog::write_log(msg, metalog::LOG_WARNING)
+#define METALOG_INFO(msg) metalog::write_log(msg, metalog::LOG_INFO)
+#define METALOG_DEBUG(msg) metalog::write_log(msg, metalog::LOG_DEBUG)
+#define METALOG_TRACE(msg) metalog::write_log(msg, metalog::LOG_TRACE)
+
 namespace metalog {
 
-enum LogLevel {
+enum log_level {
     LOG_ERROR,
     LOG_WARNING,
     LOG_INFO,
@@ -13,19 +19,9 @@ enum LogLevel {
     LOG_TRACE
 };
 
-extern bool g_logger_active;
-extern LogLevel g_logger_level;
-
-void logger_init(bool active, LogLevel level);
-void toggle_logger(bool activate);
-void set_logger_level(LogLevel level);
-void write_log(const std::string& s, LogLevel lv);
-
-#define METALOG_ERROR(msg) write_log(msg, metalog::LOG_ERROR)
-#define METALOG_WARNING(msg) write_log(msg, metalog::LOG_WARNING)
-#define METALOG_INFO(msg) write_log(msg, metalog::LOG_INFO)
-#define METALOG_DEBUG(msg) write_log(msg, metalog::LOG_DEBUG)
-#define METALOG_TRACE(msg) write_log(msg, metalog::LOG_TRACE)
+void set_logger_active(bool active);
+bool is_logger_active();
+void write_log(const std::string& s, log_level lv);
 
 }
 
